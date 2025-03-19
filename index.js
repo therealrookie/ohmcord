84062eee7a60321086f83c8327923bfcdc7db018
Robin Vollbracht <vollbrachtro84177@th-nuernberg.de>
2025-03-19 14:17:07 +0100

    Updated server for deploy 4

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const { startBot } = require("./bot/bot");
 const { startServer } = require("./website/server");
 
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 // Start Discord bot
-startBot();
+//startBot();
 
 // Start web server
-startServer();
+//startServer();
