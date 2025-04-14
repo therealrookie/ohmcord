a74a5b66f2e2d9ac8bbf4ac08c49c298b5797e68
Robin Vollbracht <vollbrachtro84177@th-nuernberg.de>
2025-04-14 11:57:38 +0200

    Updated Server for deploy 97

--- bot/bot.js
+++ bot/bot.js
@@ -1,42 +1,13 @@
-const { Client, Collection, Events, GatewayIntentBits, IntentsBitField } = require("discord.js");
+const { Collection, Events } = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
 const { handleModalInteraction, handleCommandInteraction } = require("./handleInteractions");
+const { registerCommands } = require("./register-commands");
 
 require("dotenv").config();
 
 function startBot(client) {
-  /*
-  const client = new Client({
-    intents: [
-      IntentsBitField.Flags.Guilds,
-      IntentsBitField.Flags.GuildMembers,
-      IntentsBitField.Flags.GuildMessages,
-      IntentsBitField.Flags.MessageContent,
-      GatewayIntentBits.Guilds,
-    ],
-  });
-  */
-
-  client.commands = new Collection();
-
-  const foldersPath = path.join(__dirname, "commands");
-  const commandFolders = fs.readdirSync(foldersPath);
-
-  for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
-    for (const file of commandFiles) {
-      const filePath = path.join(commandsPath, file);
-      const command = require(filePath);
-
-      if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command);
-      } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-      }
-    }
-  }
+  registerCommands(client);
 
   client.once(Events.ClientReady, (readyClient) => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
@@ -51,8 +22,6 @@ function startBot(client) {
       await handleCommandInteraction(interaction);
     }
   });
-
-  //client.login(process.env.TOKEN);
 }
 
 module.exports = { startBot };
